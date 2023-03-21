82947bc3173c147eab0a7b3f3b4f170c3b6d1f64
Luís Carlos <juniorshaannon@gmail.com>
2023-03-21 10:42:56 -0300

    Add files via upload

---
+++ PhpPOO/imprime.php
@@ -0,0 +1,7 @@
+<?php
+  include('menu.php');
+  require('msg.class.php');
+  $objeto=new Mensagem; // instanciando a classe
+  $objeto->metodo_rcb_msg(); //utilizando o metodo da classe
+
+?>
---
+++ PhpPOO/menu.php
@@ -0,0 +1,7 @@
+<?php
+    echo("<a href='index.php'> Home</a><br>");
+    echo("<a href='imprime.php'> Imprime</a><br>");
+    echo("<a href='google.com.br'> Google</a><br>");
+    echo("<a href='projecao.edu.br'> Projeção</a><br>");
+    echo("<a href='cadastro.php'> Cadastro</a>");
+?>
