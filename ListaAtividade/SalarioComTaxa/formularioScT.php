03d71b8ea8d9c920ba23355f6f96b40d6e81ab89
Luís Carlos <juniorshaannon@gmail.com>
2023-04-15 15:17:24 -0300

    Update formularioScT.php

--- ListaAtividade/SalarioComTaxa/formularioScT.php
+++ ListaAtividade/SalarioComTaxa/formularioScT.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="shortcut icon" href="img/favicon-32x32.png" type="image/x-icon">
+    <link rel="shortcut icon" href="img/money.png" type="image/x-icon">
     <link rel="stylesheet" href="formularioScT.css">
     <title>Salário Com Taxas</title>
   </head>
@@ -20,14 +20,17 @@
    $n1= @$_POST['ganho'];
    $n2= @$_POST['horas'];
    ?>
+   <form id="form1" name="form1" method="post" action="">
+          Quanto ganha por hora: <br><input type="text" name="ganho" id="txtfrase" placeholder="Digite o valor aqui..."><br>
+          Quantas horas trabalha por mês: <br>(Horas p/ semana * 4)<br><input type="text" name="horas" id="txtfrase" placeholder="Digite as horas aqui..."><br>
+          <input type="submit" name="button" id="button" value="Enviar">
+    </form> 
 
     </div>
-<div class="formuca-2">
 
-</div>
 <div class="alert alert-warning" role="alert">
     <?php
-  $obj = new SalarioSS;
+  $obj = new SalarioScT;
   $obj->calculo($n1, $n2);
   ?>
   </div>
